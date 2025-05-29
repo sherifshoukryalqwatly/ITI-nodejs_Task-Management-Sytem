72e638faab4048d17407603a5154ea35e470cea3
Abdullah Moemen <abdallahmoemen44@gmail.com>
2025-05-29 20:23:10 +0200

    chore: connext app to MongoDB using Mongoose

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-dotenv.config({ path: "config.env" });
+const mongoose = require("mongoose");
 
 const userRouter = require("./routes/userRoutes");
 const taskRouter = require("./routes/taskRoutes");
 
+dotenv.config({ path: "config.env" });
 const app = express();
+
 // general middleware
 app.use(express.json()); // parse request body
 app.use(morgan("dev")); // log info about request
 
+// connect to database
+const DB_LOCAL = process.env.DB_LOCAL;
+mongoose
+  .connect(DB_LOCAL)
+  .then(() => {
+    console.log("connected to db successful");
+  })
+  .catch((err) => {
+    console.log("error while connected to db");
+    console.log(err);
+  });
+
+// routes
 app.use("/users", userRouter);
 app.use("/tasks", taskRouter);
 
+// listen to the server
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log(`listening to the server on port ${port}`);
