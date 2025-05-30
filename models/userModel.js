f16336c54557f6bc4fdab767bc0ec1b07d6f4733
Ahmedkoraish <koraish.jr@gmail.com>
2025-05-30 16:41:45 +0300

    refactor: clean up user schema structure

--- models/userModel.js
+++ models/userModel.js
@@ -33,6 +33,6 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-const User = mongoose.model("User", userSchema,"mangmentSystemUsers");
+const User = mongoose.model("User", userSchema);
 
 module.exports = User;
