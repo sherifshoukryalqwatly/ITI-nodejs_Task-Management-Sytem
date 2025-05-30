fb59eb49afbb063dee10854562d967f0e40d6a64
Ahmedkoraish <koraish.jr@gmail.com>
2025-05-30 16:44:03 +0300

    feat: add user registration functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,36 @@
-exports.login = function (req, res) {
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
+
+exports.login = async (req, res) => {
+  const { userName, password } = req.body;
   res.send("login page");
 };
 
-exports.signUp = function (req, res) {
-  res.send("signup page");
+//registration
+exports.signUp = async (req, res) => {
+  try {
+    const { firstName, lastName, userName, password, role } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const oldUser = await User.findOne({ userName });
+    if (oldUser) {
+      return res.status(400).json({
+        status: "fail",
+        data: { title: "user already exists" },
+      });
+    }
+    const newUser = new User({
+      firstName,
+      lastName,
+      userName,
+      password: hashedPassword,
+      role,
+    });
+    await newUser.save();
+    res
+      .status(201)
+      .json({ message: `user registered with username ${userName}` });
+  } catch (err) {
+    res.status(500).json({ message: `registration failed ${err}` });
+  }
 };
